6369357c16f7328e54d2c3f018084236d6d2c2d1
leineveber <egor.leineveber@altence.com>
2022-03-23 15:24:34 +0300

    feat(night-theme): add rouned slider

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -154,3 +154,7 @@ export const hexToHSL = (hex: string): { h: number; s: number; l: number } => {
     throw new Error('Non valid HEX color');
   }
 };
+
+export const msToH = (ms: number): number => Math.floor(ms / 3600000);
+
+export const hToMS = (h: number): number => h * 3600000;
